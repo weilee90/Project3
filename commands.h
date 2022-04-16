750f2adc899cec44f74e7ea977584fa7b8a24f0f
weilee90 <73676019+weilee90@users.noreply.github.com>
2022-04-16 12:14:58 -0700

    Add files via upload

--- commands.h
+++ commands.h
@@ -5,13 +5,19 @@
 #include <sstream>
 using namespace std;
 
-void start_graph(int start, int end){
-  
+void insertionSort(int* arr, int n) {
+  int i, key, j;
+  for (i = 1; i < n; i++) {
+    key = arr[i];
+    j = i - 1;
+    
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j = j - 1;
+    }
+    arr[j + 1] = key;
+  }
 }
-
-
-
-
 
 
 #endif
--- gp.cpp
+++ gp.cpp
@@ -1,8 +1,11 @@
 #include "commands.h"
+#include "graph.h"
 #include <fstream>
 #include <iostream>
 #include <sstream>
 using namespace std;
+
+#define INF 99999999
 
 int main(int argc, char *argv[]) {
   std::fstream edges_file;
@@ -37,14 +40,13 @@ int main(int argc, char *argv[]) {
       to_add += command[i];
     }
   }
-
+  
   string line;
-  int count = 0;
+  int ver_len = 0;
   int current_year, current_vertex;
   int *vertex_label = new int[416650];
   // read data
   if (dates_file.is_open()) {
-    cout << "in dates" << endl;
     while (getline(dates_file, line)) {
       string to_add = "";
       for (int i = 0; i < line.length(); i++) {
@@ -61,22 +63,31 @@ int main(int argc, char *argv[]) {
       }
       // store the vertex in the range
       if (current_year >= start_year && current_year <= end_year) {
-        vertex_label[count] = current_vertex;
-        count++;
+        vertex_label[ver_len] = current_vertex;
+        ver_len++;
       } else if (current_year > end_year) {        
         break;
       }
     }
   }
-  
+
+  // sort the vertices
+  insertionSort(vertex_label, ver_len);
+
   line = "";
-  count = 0;
+  int adj_len = 0;
   int this_vertex, passed_vertex;
-  int **adjacency = new int*[count+1];
+  int **adjacency = new int*[ver_len];
+
+  for (int i = 0; i < ver_len; i++){
+    *(adjacency+i) = new int[ver_len];
+    for (int j = 0; j < ver_len; j++){
+      adjacency[i][j] = INF;
+    }
+  }
 
   // read edges
   if (edges_file.is_open()) {
-    cout << "in edge" << endl;
     while (getline(edges_file, line)) {
       string to_add = "";
       this_vertex = 0;
@@ -86,27 +97,26 @@ int main(int argc, char *argv[]) {
         if (line[i] == '\t' || i == line.length()-1) {
           if (time == 0) {
             this_vertex = stoi(to_add);
-            cout << "vertex = " << this_vertex;
             time++;
           } else if (i == line.length()-1) {
             to_add += line[i];
             passed_vertex = stoi(to_add);
-            cout << " linked to " << passed_vertex << endl;
           }
           to_add = "";
           i++;
         }
         to_add += line[i];
       }
-
+      
       // find the the empty spot to add the passed vertex
-      for (int i = 0; i < count; i++) {
+      for (int i = 0; i < ver_len; i++) {
         if (this_vertex == vertex_label[i]) {
-          if (adjacency[i][0] == 0) {
+          adj_len++;
+          if (adjacency[i][0] == INF) {
             adjacency[i][0] = passed_vertex;
           } else {
             int j = 1;
-            while (adjacency[i][j] != 0) {
+            while (adjacency[i][j] != INF) {
               j++;
             }
             adjacency[i][j] = passed_vertex;
@@ -115,4 +125,6 @@ int main(int argc, char *argv[]) {
       }
     }
   }
+  graph fw_graph(ver_len, adj_len, adjacency, start_year, end_year);
+
 }
